fe64ec1e849e07dd6ca8ce82be7b9b38e33159ea
Kathryn Teall <katgrace0808@gmail.com>
2020-04-25 08:55:31 -0700

    Local Storage code

--- script.js
+++ script.js
@@ -1,69 +1,88 @@
 let momentVar = moment().format('dddd, MMMM Do, YYYY h:mm a');
 let hourArr = ["8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"];
 let userText = "";
-let userInput = $("#user-input")
+let userInput = [];
 
 function simpleCalendar() {
     $(document).ready(function() {
-        currentDay();
         savedInput();
-
+        let i;
         for (let i = 0; i < hourArr.length; i++) {
             let hour = hourArr[i];
             let timeBlock = $("<main>").addClass(".time-block");
             let row = $("<section>").addClass("row");
-            let hourCol = $("<label>").addClass("col-sm-2 hour");
-            let textCol = $("<input>").addClass("col-sm-8 input past present future").attr("id", "user-input");
-            let saveCol = $("<img>").addClass("col-sm-2 saveBtn").attr({id: "save", type: "button", src: "https://cdn0.iconfinder.com/data/icons/simpline-mix/64/simpline_53-512.png"});
+            let hourCol = $("<label>").addClass(" col-md-2 hour");
+            let textCol = $(`<input class = 'col-md-8 input past present future' 'id =input-${i}>`).attr("placeholder", "Add event here.");
+            let saveCol = $(`<button class = 'col-md-2 saveBtn' 'id=hour-${i}>`).attr("type", "button");
+            let imgEl = $("<img>").attr({id: "image", src: "https://cdn0.iconfinder.com/data/icons/simpline-mix/64/simpline_53-512.png"});
             $(".container").append(timeBlock);
             timeBlock.append(row);
             row.append(hourCol);
             row.append(textCol);
             row.append(saveCol);
+            saveCol.append(imgEl);
             hourCol.text(hour);
-            console.log(hour);
+            // console.log(hour);
         }
         function currentDay(){
             let currentDay = $("#currentDay").text(momentVar);
             return currentDay;
         }
-    });
-}
+        currentDay();
+        let storedItem = [];
+
+        
+        $(".saveBtn").on("click", function(event) {
+            event.preventDefault();
+            let i;
+            let userText = $(`#input-${i}`);
+            let userInput = $(".input").val().trim();
+            let userHour = $(`#hour-${i}`);
+            storedItem.push({userHour: userText});
+            localStorage.setItem("storedItem", JSON.stringify(userInput));
+            // console.log(storedItem);
+            simpleCalendar();
+        });
+        
+
+
+// $(".saveBtn").on("click", function(event) {
+//     event.preventDefault();
+//     // let grabID = document.getElementById(saveCol.id);
+//     let i;
+//     let userInput = $(`#input-${i}`).val();
+//     // let userHour = $(`#hour-${i}`);
+//     storedItem.push({userInput, userHour});
+//     localStorage.setItem("storedItem", JSON.stringify(userInput));
+//     console.log(localStorage.getItem("storedItem"));
+//     if (userInput === "") {
+//         return;
+//     }
+//     simpleCalendar();
+// });
 
 function savedInput(){
-    let savedData = JSON.parse(localStorage.getItem("saveBtn"));
+    let savedData = JSON.parse(localStorage.getItem("storedItem"));
     if (savedData !== null) {
-        userText = savedData;
+        $(".input").text(savedData);
+        // console.log(savedData);
     }
 }
 
-function storeInput() {
-    localStorage.setItem("saveBtn", JSON.stringify(userText));
-}
-
-$(".saveBtn").on("click", function(event) {
-    event.preventDefault();
-    let userInput = userText.value;
-    if (userInput === "") {
-        return;
-    }
-
-    userText.push(userInput);
-
-    storeInput();
-    simpleCalendar();
 });
 
-function changeColor() {
-    let presentHour = moment('hour');
-    let pastHour = $(".past").moment().isBefore();
-    let futureHour = $(".future").moment().isAfter();
-//     if (moment().isBefore()) {
-//     return pastHour; 
-// }   else if (moment().isAfter()) {
-//     futureHour = true;
-// }
+
 }
+// function changeColor() {
+//     let presentHour = moment('hour');
+//     let pastHour = $(".past").moment().isBefore();
+//     let futureHour = $(".future").moment().isAfter();
+// //     if (moment().isBefore()) {
+// //     return pastHour; 
+// // }   else if (moment().isAfter()) {
+// //     futureHour = true;
+// // }
+// }
 simpleCalendar();
 
 
